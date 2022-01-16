20dd51ec57f905832e7c120d1dc4efa93eeade27
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2022-01-16 19:21:04 -0300

    adicionados: conexão com o banco de dados mysql (rodando em uma máquina windows) e pacote dotenv para manter os dados do banco seguros

---
+++ infraestrutura/conexao.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+const dotenv = require('dotenv/config')
+
+const conexao = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'agenda-petshop'
+})
+
+module.exports = conexao
