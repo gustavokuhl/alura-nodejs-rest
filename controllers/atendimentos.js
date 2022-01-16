2445598410c330a1525ad6514cf1146910fb7a01
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2022-01-16 18:27:29 -0300

    adicionada rota de post para atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,5 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos'))
+    app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e esta realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e esta realizando um POST'))
 }
