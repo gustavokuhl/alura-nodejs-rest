870e4c002d2c671da87d0b6c5bf998008ec12712
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2022-01-17 20:41:09 -0300

    adicionado tratamento de cliente e data de atendimento

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,16 +6,41 @@ class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados)
+        const dataIsValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteIsValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataIsValida,
+                mensagem: 'A data de agendamento deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteIsValido,
+                mensagem: 'O nome do cliente deve possuir ao menos 5 caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+    
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
     }
 }
 
